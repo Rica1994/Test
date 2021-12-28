b594f0300ea744435c80f6000da22e297433264d
Rica1994 <51852416+Rica1994@users.noreply.github.com>
2021-12-28 13:12:52 +0100

    Update UnityLoader.js

--- Build/UnityLoader.js
+++ Build/UnityLoader.js
@@ -1851,14 +1851,14 @@ var UnityLoader = UnityLoader || {
     Progress: {
         Styles: {
             Dark: {
-                progressLogoUrl: "https://rica1994.github.io/Test/Loadbar/Logo_SirMouse_RGB_NL.png",
-                progressEmptyUrl: "https://rica1994.github.io/Test/Loadbar/progressEmpty.Light.png",
-                progressFullUrl: "https://rica1994.github.io/Test/Loadbar/progressFull.Light.png"
+                progressLogoUrl: "https://rica1994.github.io/Test/Pictures/Logo_SirMouse_RGB_NL.png",
+                progressEmptyUrl: "https://rica1994.github.io/Test/Pictures/progressEmpty.Light.png",
+                progressFullUrl: "https://rica1994.github.io/Test/Pictures/progressFull.Light.png"
             },
             Light: {
-                progressLogoUrl: "https://rica1994.github.io/Test/Loadbar/Logo_SirMouse_RGB_NL.png",
-                progressEmptyUrl: "https://rica1994.github.io/Test/Loadbar/progressEmpty.Light.png",
-                progressFullUrl: "https://rica1994.github.io/Test/Loadbar/progressFull.Light.png"
+                progressLogoUrl: "https://rica1994.github.io/Test/Pictures/Logo_SirMouse_RGB_NL.png",
+                progressEmptyUrl: "https://rica1994.github.io/Test/Pictures/progressEmpty.Light.png",
+                progressFullUrl: "https://rica1994.github.io/Test/Pictures/progressFull.Light.png"
             }
         },
         handler: function(e, t) {
